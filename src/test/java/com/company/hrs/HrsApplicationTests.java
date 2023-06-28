54e82ade4410b844a1e6be3b41855a686c287344
asimbabayev <asimbabayev0002@gmail.com>
2023-06-28 12:42:12 +0400

    First commit

---
+++ src/test/java/com/company/hrs/HrsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.hrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
